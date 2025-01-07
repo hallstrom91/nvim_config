72c7d12d3e99950f1db36f83d59aa915311ba099
Simon Hällström <simonhallstrom@live.se>
2025-01-07 15:13:26 +0100

    nvim-lsp.lua has been terminated by simon

--- lua/plugins/nvim-lsp.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"neovim/nvim-lspconfig",
-	dependencies = { "williamboman/mason-lspconfig.nvim" },
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("config.nvim-lspconfig")
-	end,
-}
