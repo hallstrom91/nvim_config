89c2d31f5c29b514f61ff786edd546cb1e754c05
Simon Hällström <simonhallstrom@live.se>
2025-01-07 15:02:00 +0100

    bufferline.lua has been terminated by simon

--- lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	event = "BufWinEnter",
-	config = function()
-		require("config.bufferline")
-	end,
-}
