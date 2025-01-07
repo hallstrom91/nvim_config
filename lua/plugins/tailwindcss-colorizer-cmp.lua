223526d4000cdb555fce945565bffd615a1f0776
Simon Hällström <simonhallstrom@live.se>
2025-01-07 15:22:46 +0100

    tailwindcss-colorizer-cmp.lua has been terminated by simon

--- lua/plugins/tailwindcss-colorizer-cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"roobert/tailwindcss-colorizer-cmp.nvim",
-	ft = { "html", "javascript", "typescript", "css" },
-	config = function()
-		require("tailwindcss-colorizer-cmp").setup({
-			color_square_width = 2,
-		})
-	end,
-}
