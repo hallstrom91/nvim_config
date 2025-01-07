fa5165357413b254b7777707d038b63bcb54fc1c
Simon Hällström <simonhallstrom@live.se>
2025-01-07 15:15:16 +0100

    emmet.lua has been terminated by simon

--- lua/plugins/emmet.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"mattn/emmet-vim",
-	ft = { "html", "css", "javascript", "typescript", "jsx", "tsx" },
-	config = function()
-		vim.g.user_emmet_leader_key = "<C-Z>" -- leaderkey for emmet ctrl+Z
-	end,
-}
