a6e75ad5d593e8ac77462483b6296a5885030663
Simon Hällström <simonhallstrom@live.se>
2025-01-09 00:09:55 +0100

    close :Lazy with ESC-btn

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,3 +12,15 @@ require("lazy").setup({
 		},
 	},
 })
+
+local user_grp = vim.api.nvim_create_augroup("LazyUserGroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "lazy",
+	desc = "Quit lazy with <esc>",
+	callback = function()
+		vim.keymap.set("n", "<esc>", function()
+			vim.api.nvim_win_close(0, false)
+		end, { buffer = true, nowait = true })
+	end,
+	group = user_grp,
+})
