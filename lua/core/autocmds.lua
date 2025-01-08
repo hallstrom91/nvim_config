7d6e11f853d24b1843ff90cc8d411f60e45d3703
Simon Hällström <simonhallstrom@live.se>
2025-01-09 00:11:12 +0100

    update of autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -13,44 +13,26 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- auto update plugins at start
---[[ local function augroup(name)
-	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
-end
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	group = augroup("autoupdate"),
-	callback = function()
-		vim.defer_fn(function()
-			if require("lazy.status").has_updates() then
-				require("lazy").update({ show = false })
-			end
-		end, 1000)
-	end,
-}) ]]
+-- local function augroup(name)
+-- 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+-- end
+--
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	group = augroup("autoupdate"),
+-- 	callback = function()
+-- 		vim.defer_fn(function()
+-- 			if require("lazy.status").has_updates() then
+-- 				require("lazy").update({ show = false })
+-- 			end
+-- 		end, 1000)
+-- 	end,
+-- })
 
---[[ -- Auto update installs from mason
+-- Auto update Treesitter Parsers
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		vim.cmd("MasonUpdate")
-	end,
-})
-
--- Auto update Treesitter Parser
-vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = "VeryLazy",
 	callback = function()
 		vim.cmd("TSUpdate")
 	end,
-}) ]]
-
--- Close lazy UI with ESC
-local user_grp = vim.api.nvim_create_augroup("LazyUserGroup", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "lazy",
-	desc = "Quit lazy with <esc>",
-	callback = function()
-		vim.keymap.set("n", "<esc>", function()
-			vim.api.nvim_win_close(0, false)
-		end, { buffer = true, nowait = true })
-	end,
-	group = user_grp,
 })
