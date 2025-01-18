2da40b7f376ee65daa82b846eb5fedb265423f4b
Simon Hällström <simonhallstrom@live.se>
2025-01-18 23:55:02 +0100

    indent-blankline updated

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,33 +1,33 @@
-local highlight = {
-	"RainbowRed",
-	"RainbowYellow",
-	"RainbowBlue",
-	"RainbowOrange",
-	"RainbowGreen",
-	"RainbowViolet",
-	"RainbowCyan",
-}
-
-local hooks = require("ibl.hooks")
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#08a9fe" }) -- not red but babyblue-ish
-	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#6d3808" }) -- not yellow, more dark brown-ish
-	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#569CD6" })
-	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#B5CEA8" })
-	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#4EC9B0" })
-end)
-
-require("ibl").setup({
-	indent = { highlight = highlight, char = "" },
-	scope = { highlight = highlight, enabled = true, char = "󰞷" },
-	exclude = {
-		filetypes = { "dashboard" },
-		buftypes = { "nofile" },
-	},
+require('ibl').setup({
+  indent = {
+    char = '', -- Tecknet som används för indents
+    highlight = {
+      'IndentLevel1', -- Motsvarar första nivån
+      'IndentLevel2', -- Andra nivån
+      'IndentLevel3', -- Tredje nivån
+      'IndentLevel4', -- Fjärde nivån
+      'IndentLevel5', -- Femte nivån
+      'IndentLevel6', -- Sjätte nivån
+      'IndentLevel7', -- Sjunde nivån
+    },
+  },
+  scope = {
+    enabled = true,
+    char = '󰞷', -- Tecknet som används för scope
+    highlight = {
+      'IndentLevel1',
+      'IndentLevel2',
+      'IndentLevel3',
+      'IndentLevel4',
+      'IndentLevel5',
+      'IndentLevel6',
+      'IndentLevel7',
+    },
+  },
+  exclude = {
+    filetypes = { 'dashboard' }, -- Exkludera filtyper
+    buftypes = { 'nofile' }, -- Exkludera buffer-typer
+  },
 })
 
 -- indent char = "" or "" or "󰇙"
