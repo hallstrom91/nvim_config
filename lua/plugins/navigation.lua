5f1a97a402d9c869241e5c709bd6f53cdd79e573
Simon Hällström <simonhallstrom@live.se>
2025-01-18 23:55:36 +0100

    window-picker plugin added

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -5,9 +5,42 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
+      's1n7ax/nvim-window-picker',
     },
+
     config = function()
       require('config.neotree')
+    end,
+  },
+
+  {
+    's1n7ax/nvim-window-picker',
+    version = '2.*',
+    config = function()
+      vim.keymap.set('n', '<leader>w', function()
+        local picked_window_id = require('window-picker').pick_window()
+        if picked_window_id then
+          vim.api.nvim_set_current_win(picked_window_id)
+        else
+          print('Inget fönster valt!')
+        end
+      end, { desc = 'Använd window-picker för att byta fönster' })
+      require('window-picker').setup({
+        hint = 'floating-big-letter',
+        selection_chars = 'FJDKSLA;CMRUEIWOQP',
+
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { 'terminal', 'quickfix' },
+          },
+        },
+      })
     end,
   },
 
