26259fc1aa044eda5d4693c2c77be780fb9da7dc
Simon Hällström <simonhallstrom@live.se>
2025-01-22 23:08:57 +0100

    added module for git info

---
+++ lua/modules/git/git_info.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.get_current_branch()
+  local branch = vim.fn.system('git branch --show-current 2>/dev/null'):gsub('\n', '')
+  if branch and branch ~= '' then
+    return 'Branch: ' .. branch
+  else
+    return 'No active GIT repo'
+  end
+end
+
+return M
