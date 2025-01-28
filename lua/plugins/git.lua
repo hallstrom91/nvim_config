4cbe6689bb4e543a2ec1b8d64718ed9667e2fd57
Simon Hällström <simonhallstrom@live.se>
2025-01-28 12:53:37 +0100

    neogit plugin added

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,4 +15,17 @@ return {
       require('scrollbar.handlers.gitsigns').setup()
     end,
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    cmd = { 'Neogit' },
+    --keys = { '<C-ng>' },
+    config = function()
+      require('config.neogit')
+    end,
+  },
 }
